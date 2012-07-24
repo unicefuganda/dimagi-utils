58998d88d982f09a49f751b5ac7c8ae0d3ede0a5
Samuel Sekiwere <sekiskylink@gmail.com>
2012-07-24 11:13:50 +0300

    default report date range

--- dimagi/utils/decorators/datespan.py
+++ dimagi/utils/decorators/datespan.py
@@ -2,20 +2,21 @@ from datetime import datetime
 from django.http import HttpRequest, HttpResponseBadRequest
 from dimagi.utils.dates import DateSpan
 from datetime import datetime
+from django.conf import settings
 
-def datespan_in_request(from_param="from", to_param="to", 
-                        format_string="%Y-%m-%d", default_days=30, inclusive=True):
+def datespan_in_request(from_param="from", to_param="to",
+                        format_string="%Y-%m-%d", default_days=settings.LOGISTICS_DAYS_UNTIL_LATE_PRODUCT_REPORT,inclusive=True):
     """
     Wraps a request with dates based on url params or defaults and
     Checks date validity.
     """
-    
+
     # this is loosely modeled after example number 4 of decorator
     # usage here: http://www.python.org/dev/peps/pep-0318/
     def get_dates(f):
         def wrapped_func(*args, **kwargs):
             # attempt to find the request object from all the argument
-            # values, checking first the args and then the kwargs 
+            # values, checking first the args and then the kwargs
             req = None
             for arg in args:
                 if _is_http_request(arg):
@@ -31,28 +32,28 @@ def datespan_in_request(from_param="from", to_param="to",
                 def date_or_nothing(param):
                     return datetime.strptime(dict[param], format_string)\
                              if param in dict and dict[param] else None
-                try:             
+                try:
                     startdate = date_or_nothing(from_param)
                     enddate = date_or_nothing(to_param)
                 except ValueError, e:
                     return HttpResponseBadRequest(unicode(e))
                 if startdate or enddate:
                     req.datespan = DateSpan(startdate, enddate, format_string)
-                else:        
+                else:
                     # default to the last N days
                     req.datespan = DateSpan.since(default_days, format=format_string, inclusive=inclusive)
                     req.datespan.is_default = True
-                    
-            return f(*args, **kwargs) 
+
+            return f(*args, **kwargs)
         if hasattr(f, "func_name"):
             wrapped_func.func_name = f.func_name
             # preserve doc strings
-            wrapped_func.__doc__ = f.__doc__  
-            
+            wrapped_func.__doc__ = f.__doc__
+
             return wrapped_func
         else:
-            # this means it wasn't actually a view.  
-            return f 
+            # this means it wasn't actually a view.
+            return f
     return get_dates
 
 def _is_http_request(obj):
